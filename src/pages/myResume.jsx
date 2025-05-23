bdaf29fee90463c77e56928e8831e32384a1dccb
delepover56 <taha82426980@gmail.com>
2025-05-23 19:01:06 +0500

    v1.0.6

--- src/pages/myResume.jsx
+++ src/pages/myResume.jsx
@@ -1,5 +1,13 @@
 const Resume = () => {
-  return <div></div>;
+  return (
+    <section className="flex flex-col justify-start items-start gap-[30px] mb-10 mr-10">
+      <div className="max-w-[700px] w-full h-auto text-white py-18 px-8 bg-[#0f0f0f85] rounded-xl border border-[#00ff5e66] backdrop-blur-md">
+        <h1 className="merienda text-4xl text-[#00ff5e] mb-5 afterLine">
+          My Resume
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Resume;
